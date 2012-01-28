7484f0dd28d8267d489f96a27f8323f4944869af
Johnny Hom <johnnyhom@gmail.com>
2012-01-27 23:14:45 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Madparkllc::Application.config.secret_token = '43617ca118dec4bff8339159f03f82fd74fbb6014156d59a7c067596e493ce4e9dc6bc4169135b52ebdb7201dad97113352778bc8839b1ede36d28000d24ec44'
